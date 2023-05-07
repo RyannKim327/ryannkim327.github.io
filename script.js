453652640696f2e94b5924fde2249345953613ac
Mr.RySes <mpoprev2@email.com>
2023-05-07 17:29:24 +0800

    05-07-23 17:29

--- script.js
+++ script.js
@@ -19,10 +19,39 @@ window.onload = () => {
 		}
 	}, 75)
 
+	// Projects on Github
+	fetching()
+
 }
 
 function clicker(id) {
 	$(id).scrollIntoView({
 		behavior: 'smooth'
 	}, true)
+}
+
+function fetching(){
+	fetch(`https://api.github.com/users/RyannKim327/repos`).then((r) => {
+		return r.json()
+	}).then((r) => {
+		const data = r
+		let o_base =  document.createElement("div")
+		o_base.classList.add("my-lists")
+		data.map((e, i) => {
+			console.log(e)
+			let base = document.createElement("div")
+			let title = document.createElement("h3")
+			let subtitle = document.createElement("h5")
+			title.textContent = e.name
+			subtitle.textContent = e.description
+			base.classList.add("myRepo")
+			base.appendChild(title)
+			base.appendChild(subtitle)
+			o_base.appendChild(base)
+		})
+		$("#lists-projects").appendChild(o_base)
+	}).catch((e) => {
+		console.error(`Error [Github]: ${e}`)
+		$("#projects").textContent = "Something went wrong"
+	})
 }
