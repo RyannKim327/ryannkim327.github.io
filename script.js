c626130c163daba6a7c36a14a82d4acdc6c869cf
Mr.RySes <mpoprev2@email.com>
2023-06-12 14:56:58 +0800

    06-12-23 14:56

--- script.js
+++ script.js
@@ -40,14 +40,17 @@ function clicker(id) {
 
 function fetching(){
 	const colors = {
-		"java": "#ffaa00",
-		"javascript": "#ffff00",
-		"python": "#0000ff",
-		"c#": "#005000",
-		"kotlin": "#ff0090",
-		"css": "#ff00ff",
-		"html": "#ff0000",
-		"jupyter notebook": "#ffaa00"
+		"java": "#b07219",
+		"javascript": "#f1e05a",
+		"python": "#3572a5",
+		"c#": "#178600",
+		"kotlin": "#a97bff",
+		"css": "#563d7c",
+		"html": "#e34c26",
+		"jupyter notebook": "#da5b0b",
+		"php": "#4f5d95",
+		"julia": "#a270ba",
+		"c++": "#f34b7d"
 	}
 	let languages = {}
 	fetch(`https://api.github.com/users/RyannKim327/repos?sort=name&per_page=1000`).then((r) => {
@@ -77,6 +80,8 @@ function fetching(){
 				base.classList.add("myRepo")
 				if(e.language != null){
 					base.style.borderColor = colors[e.language.toLowerCase()]
+					base.style.boxShadow = `1px 1px 10px ${colors[e.language.toLowerCase()]}ef,
+					-1px -1px 5px ${colors[e.language.toLowerCase()]}`
 					base.classList.add(`${e.language.toLowerCase().replace("#", "s").replace("++", "pp").replace(/\s/, "-")}`)
 					title.textContent += " - "
 					language.textContent = `${e.language}`
