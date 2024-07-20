73825466ba3152e42e9d158814ff8cfe1e0e9f31
RyannKim327 <rksesgundo123@gmail.com>
2024-07-20 19:03:03 +0800

    07-20-24 19:02 [Fixed Location Name in iFrame map]

--- assets/script.js
+++ assets/script.js
@@ -86,6 +86,11 @@ window.onload = () => {
     }
   }
 
+  const longitude = 121.60355963623054
+  const latitude = 13.928109212658477
+  const locationName = "United Landing Homeowners Association Phase 1"
+  $("#maps").src = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56562.28002960655!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i3000!2i1800!4f13.1!3m3!1m2!1s0x33bd4b578caf4ccd%3A0x2be1e905c862fe1!2s${encodeURIComponent(locationName)}!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sen!` 
+
   languages()
   references()
 
