d59f10c19fe9c055104699ab02f65df9a50da7cd
RyannKim327 <rksesgundo123@gmail.com>
2024-08-18 13:56:53 +0800

    08-18-24 13:56

--- src/components/left.tsx
+++ src/components/left.tsx
@@ -27,31 +27,35 @@ const li = (icon: IconProp, info: string, url: string) => {
       onClick={() => {
         goto(url);
       }}
-      className={`flex items-center my-2 justify-start font-bold text-gray-300 hover:text-white ${url !== "" ? "cursor-pointer" : ""}`}
+      className={`${url === "" ? "hidden md:flex" : "flex"} items-center mx-2 my-2 justify-start font-bold text-gray-300 hover:text-white ${url !== "" ? "cursor-pointer" : ""}`}
     >
       <FontAwesomeIcon
         className="text-2xl rounded-md aspect-square p-2 box-border"
         style={styles}
         icon={icon ?? ""}
       />
-      <span className="mx-2">{info ?? ""}</span>
+      <span className="mx-2 hidden md:block">{info ?? ""}</span>
     </li>
   );
 };
 
 const Left = () => {
   return (
-    <div className="left flex flex-col items-center m-3 p-3 rounded-[5px] custom-shadow rounded">
-      <img
-        src={Me}
-        className="aspect-square w-5/12 mt-10 rounded-md mb-6"
-        alt="Ryann Kim Sesgundo"
-      />
-      <h1 className="text-2xl font-bold spartan">Ryann Kim Sesgundo</h1>
-      <h3 className="courier-prime bg-[rgba(255,255,255,0.1)] px-2 rounded-md">
-        A newbie developer
-      </h3>
-      <ul className="mt-10">
+    <div className="left flex md:flex-col sm:justify-between md:box-border md:items-center py-2 m-3 md:py-3 px-3 rounded-[5px] custom-shadow rounded">
+      <div className="flex md:flex-col items-center">
+        <img
+          src={Me}
+          className="aspect-square w-1/12 md:w-5/12 sm:mt-0 md:mt-10 rounded-md md:mb-6 mr-3 md:mr-0"
+          alt="Ryann Kim Sesgundo"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold spartan">Ryann Kim Sesgundo</h1>
+          <h3 className="flex courier-prime justify-center w-fit bg-[rgba(255,255,255,0.1)] px-2 rounded-md">
+            A newbie developer
+          </h3>
+        </div>
+      </div>
+      <ul className="md:mt-10 flex md:block">
         {li(faEnvelope, "weryses19@gmail.com", "")}
         {li(faPhone, "0985 *** 7231", "")}
         {li(faLocationDot, "Lucena City", "")}
