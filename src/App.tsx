46ed86cbf1a4a16ceef2f0c9016e8f2ed83d697a
RyannKim327 <rksesgundo123@gmail.com>
2024-09-01 09:12:06 +0800

    09-01-24 09:12

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-
 import Index from "./components/index";
 
 const App = () => {
   return (
-    <div className="bg-[#fbfff4] dark:bg-[#31363f] h-screen w-screen h-screen dark:text-white text-slate-900 transition transition-100 ease-in">
+    <div className="bg-[#fbfff4] dark:bg-[#31363f] h-dvh w-dvw dark:text-white text-slate-900 transition transition-100 ease-in">
       <Index />
     </div>
   );
