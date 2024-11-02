63e043ef5d28aeca3fc32aaf51ad87c7b3dbf105
RyannKim327 <rksesgundo123@gmail.com>
2024-11-02 13:55:49 +0800

    11-02-24 13:55

--- src/parts/header.tsx
+++ src/parts/header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import prof from "./../assets/prof.jpg";
 
 interface properties {
@@ -17,17 +17,34 @@ function List(props: list_properties) {
 }
 
 export default function Header(props: properties) {
-  const { theme, setTheme } = useState("");
+  const [scrolled, setScrolled] = useState(false);
 
-  window.onscroll = (event) => {};
+  useEffect(() => {
+    const scrollEffect = () => {
+      if (window.scrollY > 250) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollEffect);
+    return () => {
+      window.removeEventListener("scroll", scrollEffect);
+    };
+  }, []);
 
   return (
     <header
-      className={`flex flex-row w-full fixed top-0 h-12 justify-between items-center p-2 ${theme} ${props.className}`}
+      className={`flex flex-row w-full fixed top-0 h-12 justify-between items-center p-2 ${scrolled ? "bg-black text-white" : ""} ${props.className} transition ease delay-150`}
     >
       <div className="flex flex-row h-full items-center">
         <img className="aspect-square h-full rounded-full" src={prof} />
-        <h1 className="pl-2 text-lg md:text-md">Ryann Kim Sesgundo</h1>
+        <h1
+          className={`pl-2 text-lg md:text-md ${scrolled ? "opacity-1" : "opacity-0"} transition ease delay-150`}
+        >
+          Ryann Kim Sesgundo
+        </h1>
       </div>
       <nav className="flex flex-row list-none">
         <List link="">Test</List>
