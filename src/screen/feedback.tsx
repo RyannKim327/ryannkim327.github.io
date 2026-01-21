0095aa56587d342aa1d9537e9f5fab6750d4f3fe
RyannKim327 <rksesgundo123@gmail.com>
2026-01-22 05:52:31 +0800

    01-22-26  5:52

--- src/screen/feedback.tsx
+++ src/screen/feedback.tsx
@@ -9,11 +9,10 @@ export default function Feedback(props: pages_interface) {
   useEffect(() => {
     (async () => {
       const response = await get("feedback");
-      if (response.error) {
-        setSearch(true);
-      } else {
+      if (!response.error) {
         setFeedback(response.data);
       }
+      setSearch(true);
     })();
   });
 
@@ -24,25 +23,28 @@ export default function Feedback(props: pages_interface) {
     >
       <h1 className="text-base lg:text-2xl">Feedback and testimonies</h1>
       {search ? (
-        <div className="flex w-full gap-2 box-border overflow-x-auto overflow-y-hidden p-2">
+        <>
           {feedbacks.length > 0 ? (
-            <>
+            <div className="flex w-full gap-2 box-border overflow-x-auto overflow-y-hidden p-2">
               {feedbacks.map((feedback_: feedback) => {
                 <div>
                   <h1>{feedback_.application}</h1>
                   <h3>{feedback_.message}</h3>
                 </div>;
               })}
-            </>
+            </div>
           ) : (
-            <div className="flex flex-col h-full w-full items-center justify-center">
-              <p className="text-[2rem]">Please wait for a moment</p>
-              <p className="text-[1.25rem]">Backend is currently loading</p>
+            <div className="flex flex-col h-full w-full items-center justify-center gap-3">
+              <p className="text-[2rem]">No Feedback Yet</p>
+              <p className="max-w-1/3 lg:max-w-1/2 text-center text-[1.25rem]">
+                Currently, there is no feedback from any of my applications, of
+                you want to, please use my app first and give me some feedback.
+              </p>
             </div>
           )}
-        </div>
+        </>
       ) : (
-        <div className="flex flex-col h-full w-full items-center justify-center">
+        <div className="flex flex-col h-full w-full items-center justify-center gap-3">
           <p className="text-[2rem]">Please wait for a moment</p>
           <p className="text-[1.25rem]">Backend is currently loading</p>
         </div>
