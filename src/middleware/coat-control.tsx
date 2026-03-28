dcd55458ccd345527580387791e0379f7a25a512
RyannKim327 <rksesgundo123@gmail.com>
2026-03-29 06:54:00 +0800

    03-29-26  6:54

--- src/middleware/coat-control.tsx
+++ src/middleware/coat-control.tsx
@@ -3,31 +3,35 @@ import { addListener, launch } from "devtools-detector";
 import { get } from "../utils/api";
 
 interface CoatProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export default function Coat({ children }: CoatProps) {
-  const [devtoolsOpen, setDevtoolsOpen] = useState(true);
+	const [devtoolsOpen, setDevtoolsOpen] = useState(true);
 
-  useEffect(() => {
-    (async () => {
-      const res = await get("");
-      if (res.message) {
-        setDevtoolsOpen(false);
-      }
-    })();
+	useEffect(() => {
+		(async () => {
+			const res = await get("");
+			if (res.message) {
+				setDevtoolsOpen(false);
+			}
+		})();
 
-    addListener((isOpen: boolean) => {
-      location.href = "https://google.com";
-      setDevtoolsOpen(isOpen);
-    });
+		addListener((isOpen: boolean) => {
+			// location.href = "https://google.com";
+			setDevtoolsOpen(isOpen);
+		});
 
-    launch();
-  }, []);
+		launch();
+	}, []);
 
-  if (devtoolsOpen) {
-    return <div>Error</div>;
-  }
+	if (devtoolsOpen) {
+		return (
+			<div className="flex flex-col items-center justify-center text-black dark:bg-slate-950 bg-[#f9fafb] dark:text-white w-dvw h-dvh select-none">
+				Initiating Project
+			</div>
+		);
+	}
 
-  return <>{children}</>;
+	return <>{children}</>;
 }
