000da7834c178469f86378a8aa11afdfcd65001b
RyannKim327 <rksesgundo123@gmail.com>
2026-03-29 13:48:48 +0800

    03-29-26 13:48 [Added poem preview]

--- src/pages/admin/poem.tsx
+++ src/pages/admin/poem.tsx
@@ -23,7 +23,7 @@ export default function Poetry() {
     setSending(true);
     const comp = {
       title: compose.title,
-      content: compose.content.replace(/\n\n/gi, "\n").split("\n"),
+      content: compose.content.trim().replace(/\n\n/gi, "\n").split("\n"),
     };
     const data = await adminPost("poetry/submit", compose.code, comp);
     setCompose({
@@ -42,49 +42,59 @@ export default function Poetry() {
   };
 
   return (
-    <div className="flex flex-col gap-2 w-full items-start h-full">
-      <h1 className="text-center text-xl">Posting a blog content</h1>
-      <div className="flex flex-col items-center w-full h-full gap-2">
-        <Input
-          remember={true}
-          name="title"
-          value={compose}
-          onChange={setCompose}
-        >
-          Title
-        </Input>
-        <Input
-          type="password"
-          name="code"
-          value={compose}
-          onChange={setCompose}
-        >
-          Code
-        </Input>
-        <div className="flex flex-col border border-solid border-[#0c0c0c] dark:border-[#f9f9f6] w-full h-full rounded-sm">
-          <span className="pl-4">Content</span>
-          <textarea
-            className="w-full border-none outline-none px-2 h-full"
-            name=""
-            id=""
-            value={compose.content}
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-              setCompose((prev) => ({
-                ...prev,
-                content: event.target.value,
-              }));
-              local("content", event.target.value ?? "");
-            }}
-          ></textarea>
-        </div>
-        {!sending ? (
-          <button
-            onClick={submitPoem}
-            className="border p-2 border-solid border-[#0c0c0c] dark:border-[#f9f9f6] w-full rounded-sm"
+    <div className="flex flex-row gap-2 w-full items-start h-full">
+      <div className="flex flex-col gap-2 w-full items-start h-full">
+        <h1 className="text-center text-xl">Posting a blog content</h1>
+        <div className="flex flex-col items-center w-full h-full gap-2">
+          <Input
+            remember={true}
+            name="title"
+            value={compose}
+            onChange={setCompose}
           >
-            Submit
-          </button>
-        ) : null}
+            Title
+          </Input>
+          <Input
+            type="password"
+            name="code"
+            value={compose}
+            onChange={setCompose}
+          >
+            Code
+          </Input>
+          <div className="flex flex-col border border-solid border-[#0c0c0c] dark:border-[#f9f9f6] w-full h-full rounded-sm">
+            <span className="pl-4">Content</span>
+            <textarea
+              className="w-full border-none outline-none px-2 h-full"
+              name=""
+              id=""
+              value={compose.content}
+              onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                setCompose((prev) => ({
+                  ...prev,
+                  content: event.target.value,
+                }));
+                local("content", event.target.value ?? "");
+              }}
+            ></textarea>
+          </div>
+          {!sending ? (
+            <button
+              onClick={submitPoem}
+              className="border p-2 border-solid border-[#0c0c0c] dark:border-[#f9f9f6] w-full rounded-sm"
+            >
+              Submit
+            </button>
+          ) : null}
+        </div>
+      </div>
+      <div className="hidden md:flex flex-col gap-2 items-start overflow-hidden h-full w-1/2">
+        <span>{compose.title}</span>
+        <div className="h-full w-full overflow-y-auto wrap-break-word">
+          {compose.content.split("\n").map((content) => {
+            return <p>{content}</p>;
+          })}
+        </div>
       </div>
       <ToastContainer />
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Admin() {
     <AdminController>
       <div className="flex flex-col md:flex-row justify-center items-center p-4 bg-[#f6f6f6] text-black dark:bg-[#0c0c0c] dark:text-white w-dvw h-dvh gap-2 select-none">
         <div
-          className={`flex flex-col ${toggle ? "h-15 overflow-y-hidden" : "h-1/2"} transition-all delay-150 md:h-full w-full md:w-1/6 justify-between bg-zinc-300 text-black dark:bg-slate-800 dark:text-white p-5 rounded`}
+          className={`flex flex-col ${toggle ? "h-15 overflow-y-hidden" : "h-1/2"} transition-all delay-150 md:h-full w-full md:w-[calc(25.0%-0.5rem)] justify-between bg-zinc-300 text-black dark:bg-slate-800 dark:text-white p-5 rounded`}
         >
           <div className="flex flex-col gap-3">
             <div className={`flex flex-row w-full justify-between`}>
