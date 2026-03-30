4e57c1dc02f446347352af9824aaaed96b728484
RyannKim327 <rksesgundo123@gmail.com>
2026-03-30 13:31:19 +0800

    03-30-26 13:31 [Making resume reusable component]

--- src/pages/admin/personal-info.tsx
+++ src/pages/admin/personal-info.tsx
@@ -1,35 +1,6 @@
-import Profile from "./../../assets/light.png";
-
-interface headerprops {
-  name: string;
-}
-
-interface experienceProps {
-  year: number;
-  title: string;
-}
-
-const Header = ({ name }: headerprops) => {
-  return (
-    <div className="flex flex-row items-center gap-1">
-      <span className="font-mono tracking-[0.33rem] text-[0.66em]">
-        {name.toUpperCase()}
-      </span>
-      <div className="flex w-full bg-green-400 p-[1px]"></div>
-    </div>
-  );
-};
-
-const ExperienceElement = (props: experienceProps) => {
-  return (
-    <div className="flex flex-col">
-      <span className="text-[0.75rem] italic font-serif font-bold">
-        {props.year}
-      </span>
-      <span className="ml-2 text-[0.90rem] font-mono">{props.title}</span>
-    </div>
-  );
-};
+import { useState } from "react";
+import Resume from "../../components/resume";
+import Input from "../../widgets/input";
 
 const experiences = [
   {
@@ -75,89 +46,31 @@ const experiences = [
 ];
 
 export default function PersonalInfo() {
+  const [user, setUser] = useState({
+    first_name: "Ryann Kim",
+    middle_name: "Malabanan",
+    last_name: "Sesgundo",
+    position: "Multi-Stack Developer",
+    phone: "+63912 345 6789",
+    email: "weryses19@gmail.com",
+    address: "Lucena City",
+    about:
+      "A full-stack developer with years of experience in different field such as Web Development and Mobile Development. With full compassion and dedication on work gained from being student assistant.",
+  });
+
   return (
     <div className="flex flex-col md:flex-row gap-2 w-full h-full">
-      <div className="w-[calc(40%-0.5rem)] ">side A</div>
-      <div className="w-[calc(60%-0.5rem)] ">
-        <span>Preview</span>
-        <div className="flex flex-col bg-white text-black aspect-[210/297]">
-          <div className="flex flex-row items-center justify-between py-1 px-4">
-            <div className="flex items-center gap-2">
-              <div className="w-15 aspect-square overflow-hidden rounded-full border border-solid border-black">
-                <img className="w-full -mt-2" src={Profile} alt="Pic" />
-              </div>
-              <div className="flex flex-col max-w-2/3">
-                <span className="font-serif">Ryann Kim M. Sesgundo</span>
-                <span className="font-stretch-semi-condensed text-[0.75rem] tracking-[0.25rem]">
-                  A Multi-Stack Developer
-                </span>
-              </div>
-            </div>
-            <div className="flex flex-col font-serif italic">
-              <span>email@gmail.com</span>
-              <span>09123456789</span>
-              <span>Lucena City</span>
-            </div>
-          </div>
-          <div className="flex flex-row border-t w-full border-black border-solid h-full">
-            <div className="flex flex-col p-3 w-full gap-3">
-              <p className="italic text-sm text-justify font-serif border-l-3 border-green-500 border-solid p-2 pl-3">
-                A full-stack developer with years of experience in different
-                field such as Web Development and Mobile Development. With full
-                compassion and dedication on work gained from being student
-                assistant.
-              </p>
-              <Header name="experience" />
-              <div>
-                {experiences.map((exp) => {
-                  return (
-                    <ExperienceElement year={exp.year} title={exp.title} />
-                  );
-                })}
-              </div>
-            </div>
-            <div className="flex flex-col bg-gray-100 border-l border-black border-solid p-3 w-[calc(50%-0.5rem)] gap-3">
-              <Header name="Skills" />
-              <ul className="flex flex-col gap-1 text-[0.66rem]">
-                <li>Project Manager</li>
-                <li>
-                  Back-end Development
-                  <ul className="ml-2">
-                    <li>Django</li>
-                    <li>Flask</li>
-                    <li>Go GIN</li>
-                    <li>ExpressJS</li>
-                    <li>PHP</li>
-                  </ul>
-                </li>
-                <li>
-                  Front-end Development
-                  <ul className="ml-2">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Javascript</li>
-                    <li>Typescript</li>
-                    <li>ReactJS</li>
-                  </ul>
-                </li>
-                <li>
-                  Prototype
-                  <ul className="ml-2">
-                    <li>Figma</li>
-                  </ul>
-                </li>
-                <li>
-                  Android Development
-                  <ul className="ml-2">
-                    <li>Java</li>
-                    <li>Kotlin</li>
-                    <li>React Native</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-col w-[calc(40%-0.5rem)] gap-2 overflow-y-auto">
+        {Object.keys(user).map((key) => {
+          return (
+            <Input value={user} onChange={setUser} name={key}>
+              {key.toUpperCase().replace(/_/g, " ")}
+            </Input>
+          );
+        })}
+      </div>
+      <div className="w-[calc(60%-0.5rem)] overflow-y-auto">
+        <Resume user={user} data={experiences} />
       </div>
     </div>
   );
