72f94771823d966e90df2a6cd22d9b900ea5f3ee
RyannKim327 <rksesgundo123@gmail.com>
2026-04-16 15:24:06 +0800

    04-16-26 15:24

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Admin() {
     <AdminController>
       <div className="flex flex-col md:flex-row justify-center items-center p-4 w-full h-full gap-2 select-none">
         <AdminSidebar />
-        <div className="flex flex-col h-full w-full md:flex-1 p-5 rounded overflow-y-scroll">
+        <div className="flex flex-col bg-slate-900 h-full w-full md:flex-1 p-5 rounded overflow-y-scroll">
           <Outlet />
         </div>
       </div>
