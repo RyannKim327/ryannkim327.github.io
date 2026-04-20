a221fb5c2f63491a4d9df3741e21f592d94b0818
RyannKim327 <rksesgundo123@gmail.com>
2026-04-20 22:42:46 +0800

    04-20-26 22:42 [feat: resume]

--- src/routes/admin/info.svelte
+++ src/routes/admin/info.svelte
@@ -1,3 +1,12 @@
-<div>
-Info
+<script>
+	import Resume from "@/components/resume.svelte";
+</script>
+
+<div class="flex flex-row">
+	<div class="w-full">
+		<input type="text" />
+	</div>
+	<div class="md:w-[calc(50%-0.5rem)]">
+		<Resume />
+	</div>
 </div>
