bd2897822f543a25c48a361e477427c8b2509f93
RyannKim327 <rksesgundo123@gmail.com>
2026-06-18 05:24:56 +0800

    06-18-26  5:24 [-m]

--- src/routes/home/feedback.svelte
+++ src/routes/home/feedback.svelte
@@ -1,35 +1,40 @@
 <script lang="ts">
-	import Card from "@/components/card.svelte";
-	import Loader from "@/components/loader.svelte";
+  import Card from "@/components/card.svelte";
+  import Loader from "@/components/loader.svelte";
 
-	let {
-		feedbacks = [],
-		parseData = false,
-	}: { feedbacks?: Record<string, any>[]; parseData?: boolean } = $props();
+  let {
+    feedbacks = [],
+    parseData = false,
+  }: { feedbacks?: Record<string, any>[]; parseData?: boolean } = $props();
 </script>
 
 <div
-	id="feedback"
-	class="flex flex-wrap gap-2 p-2 pt-[25%] md:pt-[calc(5%+0.5rem)] h-full w-full overflow-y-auto snap-start"
+  id="feedback"
+  class="flex flex-wrap gap-2 p-2 pt-[25%] md:pt-[calc(5%+0.5rem)] h-full w-full items-start justify-center overflow-y-auto snap-start"
 >
-	{#if feedbacks.length > 0 && parseData}
-		{#each feedbacks as feedback}
-			<Card
-				class="aspect-video w-full md:w-[calc(33.333%-0.5rem)] justify-between"
-			>
-				<span class="font-bold text-[1.25rem]">{feedback.name}</span>
-				<span class="italic text-[0.75rem]">{feedback.content}</span>
-			</Card>
-		{/each}
-	{:else if parseData}
-		<div>
-			<span>No Feedback submitted</span>
-		</div>
-	{:else}
-		{#each Array(6) as _, i (i)}
-			<Loader
-				class="aspect-video w-full md:w-[calc(33.333%-0.5rem)] justify-between"
-			></Loader>
-		{/each}
-	{/if}
+  {#if feedbacks.length > 0 && parseData}
+    {#each feedbacks as feedback}
+      <Card
+        class="aspect-video w-full md:w-[calc(33.333%-0.5rem)] justify-start gap-2"
+      >
+        <div class="flex flex-col">
+          <span class="font-bold text-[1.5rem]">{feedback.application}</span>
+          <span class="text-[0.75rem]"
+            >{feedback.userId ?? "No user provided"}</span
+          >
+        </div>
+        <span class="italic text-[1rem]">{feedback.message}</span>
+      </Card>
+    {/each}
+  {:else if parseData}
+    <div>
+      <span>No Feedback submitted</span>
+    </div>
+  {:else}
+    {#each Array(6) as _, i (i)}
+      <Loader
+        class="aspect-video w-full md:w-[calc(33.333%-0.5rem)] justify-between"
+      ></Loader>
+    {/each}
+  {/if}
 </div>
