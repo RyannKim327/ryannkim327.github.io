da44b4ec31e95663c7717cf7725c1e2f22593748
RyannKim327 <rksesgundo123@gmail.com>
2026-08-18 14:28:44 +0800

    08-18-26 14:28 [chore: footer]

--- src/components/index/footer.tsx
+++ src/components/index/footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <div id="footer" className="flex flex-col justify-center bg-bg w-full h-[50%] px-5 gap-3 border-t border-t-solid border-t-fg">
-      <div className="flex items-start w-full">
-        <div className="flex flex-col gap-2 w-[calc(50%-0.5rem)]">
+      <div className="flex flex-col-reverse lg:flex-row gap-5 lg:gap-0 justify-center items-center lg:items-start w-full">
+        <div className="flex flex-col gap-2 items-center lg:items-start w-[calc(100%-1rem)] lg:w-[calc(50%-0.5rem)]">
           <span>Socials</span>
-          <div className="flex flex-col">
+          <div className="flex lg:flex-col gap-2">
             <span
               className="cursor-pointer"
               onClick={() => {
@@ -22,12 +22,15 @@ export default function Footer() {
               }}>Wakatime</span>
           </div>
         </div>
-        <div className="flex flex-col gap-1 w-[calc(50%-0.5rem)]">
+        <div className="flex flex-col gap-2 w-[calc(100%-1rem)] lg:w-[calc(50%-0.5rem)]">
           <span>Direct Contact Here</span>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email Address" />
-          <textarea placeholder="Message"></textarea>
+          <div className="flex flex-wrap gap-2">
+            <input className="flex-1 w-full bg-input p-2 rounded" type="text" placeholder="Name" />
+            <input className="flex-1 w-full bg-input p-2 rounded" type="email" placeholder="Email Address" />
+            <textarea className="fex-1 w-full bg-input p-2 rounded" placeholder="Message"></textarea>
+          </div>
           <input type="button" value="Send Message" className="card" />
+
         </div>
       </div>
       <span className="w-full text-center font-mono py-3">All rights reserved ©{new Date().getFullYear()}</span>
