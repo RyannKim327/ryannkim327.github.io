c1cf902ccc00cdf5f83d6d4f814e1fd25864b60f
RyannKim327 <rksesgundo123@gmail.com>
2026-08-19 00:20:29 +0800

    08-19-26  0:20 [chore: endpoints]

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './app.tsx'
 import { HashRouter, Route, Routes } from 'react-router'
 import Admin from './admin.tsx'
+import AdminDashboard from '@/routes/admin/index.tsx'
+import AdminExperiences from '@/routes/admin/experiences.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <Routes>
         <Route path="" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/" element={<Admin />}>
+          <Route path="" element={<AdminDashboard />} />
+          <Route path="experiences" element={<AdminExperiences />} />
+        </Route>
       </Routes>
     </HashRouter>
   </StrictMode>,
