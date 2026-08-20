a6744386c3f8d7e24c595e00056cbf7f97dc59c5
RyannKim327 <rksesgundo123@gmail.com>
2026-08-20 16:58:34 +0800

    08-20-26 16:58

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './user.tsx'
+import App from './app.tsx'
 import { HashRouter, Route, Routes } from 'react-router'
 import Admin from './admin.tsx'
 import AdminDashboard from '@/routes/admin/index.tsx'
